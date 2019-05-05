63df1fca37a573dc8975dadca9a35690c2ae6f4f
George Mekhouzla <gorik_mcse@hotmail.com>
2019-05-04 23:51:21 -0400

    Animated Landing Page

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(function () {
+    $('.collapse').on('show.bs.collapse', function () {
+        $(this).prev().find('.fa').removeClass().addClass('fa fa-minus-circle');
+    });
+    
+    $('.collapse').on('hide.bs.collapse', function () {
+        $(this).prev().find('.fa').removeClass().addClass('fa fa-plus-circle');
+    });
+    
+    $('#myForm').validator();
+
+    new WOW().init();
+});
